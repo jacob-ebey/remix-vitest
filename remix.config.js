f5fe05615a6ed828a93cdb6e1f9851afba72c96d
Jacob Ebey <jacob.ebey@live.com>
2022-06-28 18:49:18 -0700

    updated remix config

--- remix.config.js
+++ remix.config.js
@@ -3,7 +3,5 @@ const { flatRoutes } = require("remix-flat-routes");
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/*"],
-  routes: async (defineRoutes) => {
-    return flatRoutes("routes", defineRoutes);
-  },
+  routes: (defineRoutes) => flatRoutes("routes", defineRoutes),
 };
